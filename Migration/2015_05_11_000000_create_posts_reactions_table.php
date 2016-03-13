d3b45a60f503b7132498f87fe9e5f8be319e6005
Jordan Schnaidt <jordanschnaidt@gmail.com>
2016-03-13 12:59:46 -0500

    Helps to add the files.

---
+++ Migration/2015_05_11_000000_create_posts_reactions_table.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * This file is part of Flarum.
+ *
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/* For Beta 5
+use Flarum\Database\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+return Migration::createTable(
+    //#DEBUG 'posts_likes',
+    'posts_reactions',
+    function (Blueprint $table) {
+        $table->integer('post_id')->unsigned();
+        $table->integer('user_id')->unsigned();
+        $table->primary(['post_id', 'user_id']);
+    }
+);
+*/
+
+namespace Flarum\Reactions\Migration;
+
+use Flarum\Database\AbstractMigration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreatePostsReactionsTable extends AbstractMigration
+{
+    public function up()
+    {
+        $this->schema->create('posts_reactions', function (Blueprint $table) {
+            $table->integer('post_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->primary(['post_id', 'user_id']);
+        });
+    }
+    public function down()
+    {
+        $this->schema->drop('posts_reactions');
+    }
+}
---
+++ Migration/2015_09_04_000000_add_default_reaction_permissions.php
@@ -0,0 +1,71 @@
+<?php
+/*
+ * This file is part of Flarum.
+ *
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/* For Beta 5
+use Flarum\Core\Group;
+use Flarum\Core\Permission;
+
+$getPermissionAttributes = function () {
+    return [
+        'group_id' => Group::MEMBER_ID,
+        //#DEBUG 'permission' => 'discussion.likePosts',
+        'permission' => 'discussion.reactToPosts',
+    ];
+};
+
+return [
+    'up' => function () use ($getPermissionAttributes) {
+        Permission::unguard();
+
+        $permission = Permission::firstOrNew($getPermissionAttributes());
+
+        $permission->save();
+    },
+
+    'down' => function () use ($getPermissionAttributes) {
+
+        Permission::where($getPermissionAttributes())->delete();
+    }
+];
+*/
+
+namespace Flarum\Reactions\Migration;
+
+use Flarum\Core\Group;
+use Flarum\Core\Permission;
+use Flarum\Database\AbstractMigration;
+
+class AddDefaultReactToPermissions extends AbstractMigration
+{
+    public function up()
+    {
+        Permission::unguard();
+
+        $permission = Permission::firstOrNew($this->getPermissionAttributes());
+
+        $permission->save();
+    }
+
+    public function down()
+    {
+        Permission::where($this->getPermissionAttributes())->delete();
+    }
+
+    /**
+     * @return array
+     */
+    protected function getPermissionAttributes()
+    {
+        return [
+            'group_id' => Group::MEMBER_ID,
+            'permission' => 'discussion.reactToPosts'
+        ];
+    }
+}
